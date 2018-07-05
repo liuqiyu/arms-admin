6a8865d1e5ff6707204f916be9c4294967c957a8
1064839051@qq.com <1064839051@qq.com>
2018-07-05 16:34:58 +0800

    '123'

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var router = require('./routes/index');
 
 var app = express();
 
-//设置跨域访问
+//设置跨域访问/
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
