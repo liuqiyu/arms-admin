a4b735d655fd3e49064eeb3d18bd19537649c5c0
1064839051@qq.com <1064839051@qq.com>
2018-07-10 14:23:08 +0800

    '跨域问题'

--- app.js
+++ app.js
@@ -8,17 +8,26 @@ var router = require('./routes/index');
 var app = express();
 
 //设置跨域访问/
+// app.all('*', function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("X-Powered-By",' 3.2.1');
+//   // res.header("Content-Type", "application/json;charset=utf-8");
+//   // 将外源设为指定的域，比如：http://localhost:8080
+//   res.header('Access-Control-Allow-Origin', "http://localhost:8080");
+//   // 将Access-Control-Allow-Credentials设为true
+//   res.header('Access-Control-Allow-Credentials', true); // 可以带cookies
+//   next();
+// });
+//设置跨域访问
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1');
-  // res.header("Content-Type", "application/json;charset=utf-8");
-  // 将外源设为指定的域，比如：http://localhost:8080
-  res.header('Access-Control-Allow-Origin', "http://localhost:8080");
-  // 将Access-Control-Allow-Credentials设为true
-  res.header('Access-Control-Allow-Credentials', true); // 可以带cookies
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
 });
 
 // view engine setup
